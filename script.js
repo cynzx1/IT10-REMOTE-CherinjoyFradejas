c93050c1df05340e9374071972bdb403fdc1547e
che <kwiniii2004@gmail.com>
2025-05-14 10:37:21 +0800

    Added JavaScript to handle form submit and assign correct name variable

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("nameForm").addEventListener("submit",function (event){
+    event.preventDefault();
+
+    const nameInput = document.getElementById("name").getElementById("name").value.trim(0);
+    const correctname = "che";
+});
